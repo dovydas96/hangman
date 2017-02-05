40960d141a632d335223cad84549ff0d049f9016
Dovydas <Laptop@localhost.localdomain>
2017-02-05 15:34:00 +0000

    Fixed

--- Start.py
+++ Start.py
@@ -1,16 +1,7 @@
 import Worked as wo
 
-def get_word():
-    user_word = input("Player 1, enter your word:")
-    user_word = user_word.upper()
-    if not all(x.isalpha() or x.isspace() for x in user_word):
-        print("This is not a valid word,try again")
-        return get_word()
-    print("\n"*2000)
-    return user_word
-
 def main():
-    original = get_word()
+    original = wo.get_word()
     hidden = wo.hide_word(original)
 
     seen = []
@@ -29,7 +20,8 @@ def main():
             elif (wo.checker(letter,original,list(hidden))):
                 hidden = wo.checker(letter,original,list(hidden))
                 seen.append(letter)
-                if hidden == original:
+
+                if wo.check_word(original,hidden):
                     print("Correct!\nThe word was {}".format(original))
                     quit()
                 else:
